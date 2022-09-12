d16fdaf79f06f43b79e095b02821cf1395d28712
Banasing-Sahadatu <110116966+Banasing-Sahadatu@users.noreply.github.com>
2022-09-12 14:52:50 +0000

    commit 1

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/userSchema')
+const bcrypt = require('bcrypt');
+const { validate } = require('../config/Validator');
+
+// adding a user
+const addUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    const valid = await validate({  username, email, password });
+    if (valid) {
+
+        const hashedPassword = await bcrypt.hash(valid.password, 8)
+        const savedUser = await User.create({
+            username,
+            email,
+            password:hashedPassword,
+
+        });
+        res.status(201).json({
+            success: true,
+            message: "user created",
+            savedUser,
+        });
+    } else {
+        res.status(400).json({
+            error: true,
+            message: "invalid data"
+        });
+    }
+};
+
+module.exports ={addUser}
